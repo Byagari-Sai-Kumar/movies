be235c433d946770baf9fd1a4805c0ab76e33141
Sai-Kumar-Byagari <sai815538@gmail.com>
2023-05-30 10:10:26 +0000

    All APIs

--- app.js
+++ app.js
@@ -61,3 +61,88 @@ app.post("/movies/", async (request, response) => {
 
   response.send("Movie Successfully Added");
 });
+
+//get a single movie API3
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movie_id } = request.params;
+
+  const getMovieQuery = `
+    SELECT
+    * 
+    FROM 
+    movie
+    WHERE 
+    movie_id = ${movie_id};`;
+
+  const movie = await db.get(getMovieQuery);
+
+  response.send(movie);
+});
+
+//update movie API4
+app.put("/movies/:movieId/", async (request, response) => {
+  const { movie_id } = request.params;
+
+  const movieDetails = request.body;
+
+  const { director_id, movie_name, lead_actor } = movieDetails;
+
+  const updateMovieQuery = `
+    UPDATE movie
+    SET 
+        director_id = ${director_id},
+        movie_name = ${movie_name},
+        lead_actor = ${lead_actor}
+    WHERE 
+        movie_id = ${movie_id};`;
+
+  await db.run(updateMovieQuery);
+
+  response.send("Movie Details Updated");
+});
+
+//Delete movie API5
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movie_id } = request.params;
+
+  const deleteMovieQuery = `
+    DELETE FROM
+        movie
+    WHERE 
+        movie_id = ${movie_id};`;
+
+  await db.run(deleteMovieQuery);
+
+  response.send("Movie Removed");
+});
+
+//Get directors API6
+app.get("/directors/", async (request, response) => {
+  const getDirectorsQuery = `
+    SELECT
+    * 
+    FROM 
+    director
+    ;`;
+
+  const directors = await db.all(getDirectorsQuery);
+
+  response.send(directors);
+});
+
+//get director movies API7
+app.get("/directors/:directorId/movies/", async (request, response) => {
+  const { director_id } = request.params;
+
+  const getDirectorMoviesQuery = `
+    SELECT
+        movie_name
+    FROM 
+        movie
+    WHERE 
+        director_id = ${director_id};`;
+
+  const directorMovieList = await db.all(getDirectorMoviesQuery);
+
+  response.send(directorMovieList);
+});
